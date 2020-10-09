4902e53c8f52ba429f9007bd16f2abbcbeb39ace
KACPER CIERPIAK <KACPER_CIERPIAK@jabil.com>
2020-10-09 18:42:34 +0200

    Init Project

---
+++ Test projects/NauralGasLib/Services/EmptyService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NauralGasLib.Services
+{
+    class EmptyService
+    {
+    }
+}
---
+++ Test projects/NeuralGas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuralGas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
